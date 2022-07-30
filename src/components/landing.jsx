8b655f34e0adfde95f6325c2447542ed2fb61ec1
Dennis Eghtedari <year_2000_@hotmail.com>
2022-07-30 12:25:55 +0100

    no unused vars

--- src/components/landing.jsx
+++ src/components/landing.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React from "react";
 import Nav from "./nav";
 import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -1,6 +1,5 @@
 import React, {useState} from "react";
 import { Link } from "react-router-dom";
-import logo from "../assets/logo.png";
 import invert from "../assets/logoinvert.png"
 
 export default function Nav() {
